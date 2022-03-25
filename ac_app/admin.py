2e73c2ce6eb1b342e5c8a23c6a7d5dfc0cf98f89
vera <lerika250293@mail.ru>
2022-03-25 18:00:19 +0300

    all'

--- ac_app/admin.py
+++ ac_app/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import Accaunt
+from .models import Account
 # Register your models here.
 
-class AccauntAdmin(admin.ModelAdmin):
+class AccountAdmin(admin.ModelAdmin):
     list_display = ['login', 'status', 'created_at']
     list_editable = ['status']
     ordering = ["login"]
     list_per_page = 10
     search_fields = ['login']
 
-admin.site.register(Accaunt, AccauntAdmin)
+admin.site.register(Account, AccountAdmin)
 
 
 # Register your models here.
--- ac_app/models.py
+++ ac_app/models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from django.db import models
 
-class Accaunt(models.Model):
+class Account(models.Model):
     login = models.CharField(max_length=70, default='')
     status = models.CharField(max_length=70, default='')
     created_at = models.DateTimeField(default=datetime.now, null=True, blank=True, editable=False)
+
 
 
